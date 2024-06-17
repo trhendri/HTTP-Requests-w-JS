c90202dea9908b1dbfd573d8b6196c857b201632
Talise Hendrix <trhendri1@gmail.com>
2024-06-17 11:03:33 -0500

    add tests status code and body contain

--- tests/getHandlers.test.js
+++ tests/getHandlers.test.js
@@ -1,10 +1,25 @@
 // eslint-disable-next-line no-undef
 const config = require('../config');
 
-test('', async () => {
+test('should return 200 status code', async () => {
+	let actualStatusCode;
 	try {
-		const response = await fetch(`${config.API_URL}/your/endpoint`);
+		const response = await fetch(`${config.API_URL}/api/v1/kits?cardId=1`);
+		actualStatusCode = response.status;
 	} catch (error) {
 		console.error(error);
 	}
+	expect(actualStatusCode).toBe(200);
+});
+
+test('Body should contain', async () => {
+	let actualResponseBody;
+	try {
+		const response= await fetch(`${config.API_URL}/api/v1/kits?cardId=1`);
+		actualResponseBody = await response.json();
+	} catch (error) {
+		console.error(error);
+	}
+
+	expect(actualResponseBody[0]['name']).toContain('For picnic');
 });
